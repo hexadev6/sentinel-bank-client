8102af4a70799055142d97ab209cbcd29375bc05
Israt85 <isratanjuman3@gmail.com>
2024-01-18 18:27:01 +0600

    added FAQ section

--- src/pages/MainlayoutPages/Home/Home.jsx
+++ src/pages/MainlayoutPages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../../components/Home/Banner/Banner";
+import FaqSection from "../../../components/Home/FaqSection/FaqSection";
 
 
 
@@ -6,6 +7,7 @@ const Home = () => {
     return <>
     this is hompage
         <Banner/>
+        <FaqSection></FaqSection>
     </>
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,6 @@ export default withMT({
         }
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 })
 
