37ca206ca0fd5ea5806a33903a5b5eb0f0a34e96
RSsunny <sunnysharif154@gmail.com>
2024-01-19 16:49:37 +0600

    update route

--- src/components/About us/AboutCompany/AboutCompany.jsx
+++ src/components/About us/AboutCompany/AboutCompany.jsx
@@ -1,31 +1,40 @@
-import img1 from '../../../assets/banner/teamwork (3).jpg'
-import img2 from '../../../assets/banner/teamwork.jpg'
-import Container from '../../Shared/container/Container';
+import img1 from "../../../assets/banner/teamwork (3).jpg";
+import img2 from "../../../assets/banner/teamwork.jpg";
+import Container from "../../Shared/container/Container";
 
 const AboutCompany = () => {
-    return (
-        <Container>
-             <p className=' text-[#4d774e] text-center mb-4 text-lg'>About Company</p>
-           <div className='flex flex-col lg:flex-row mx-6 justify-around gap-6'>
-           
-           <div  className='sm:mx-10'>
-                <div className='relative'>
-                <img className='h-60 w-96' src={img2} alt="" />
-                <img className='ml-40 -mt-32 h-60 w-96' src={img1} alt="" />
-                </div>
-                <div className='absolute hover:scale-110 -mt-32 mx-6 flex gap-2 p-6 bg-white border-4 md:w-60 lg:w-1/4 border-l-[#4d774e]'>
-                    <h2 className='text-3xl text-red-600 font-bold' >25</h2>
-                    <h2 className='w-40 text-center'>We Have More Than years of Experience</h2>
-                </div>
-
-            </div>
-            <div className='w-full lg:w-1/2 px-10'>
-                  <h2 className='text-2xl font-bold'>Crafting Excellence Since 25 Years</h2>
-                  <p className='text-xl font-medium'>For 25 years, Sentinel Trust Bank has been synonymous with excellence. Since our inception, we have continually evolved to meet the dynamic needs. Our seasoned team of professionals brings a wealth of experience, ensuring that every project we undertake is executed with precision, creativity, and a commitment to surpassing expectations.</p>
-            </div>
-           </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <p className=" text-[#4d774e] text-center mb-4 text-lg">About Company</p>
+      <div className="flex flex-col lg:flex-row mx-6 justify-around gap-6">
+        <div className="sm:mx-10">
+          <div className="relative">
+            <img className="h-60 w-96" src={img2} alt="" />
+            <img className="ml-40 -mt-32 h-60 w-96" src={img1} alt="" />
+          </div>
+          <div className="absolute hover:scale-110 -mt-32 mx-6 flex gap-2 p-6 bg-white border-4 md:w-60 lg:w-1/4 border-l-[#4d774e]">
+            <h2 className="text-3xl text-red-600 font-bold">25</h2>
+            <h2 className="w-40 text-center">
+              We Have More Than years of Experience
+            </h2>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 px-10">
+          <h2 className="text-2xl font-bold">
+            Crafting Excellence Since 25 Years
+          </h2>
+          <p className="text-xl font-medium">
+            For 25 years, Sentinel Trust Bank has been synonymous with
+            excellence. Since our inception, we have continually evolved to meet
+            the dynamic needs. Our seasoned team of professionals brings a
+            wealth of experience, ensuring that every project we undertake is
+            executed with precision, creativity, and a commitment to surpassing
+            expectations.
+          </p>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default AboutCompany;
