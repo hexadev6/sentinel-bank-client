94cd358d692cacaefdc70577e78f8d9e48f5b161
Israt85 <isratanjuman3@gmail.com>
2024-01-19 15:50:43 +0600

    fixed responsive issues

--- src/components/About us/ReadyToTalk.jsx/ReadyToTalk.jsx
+++ src/components/About us/ReadyToTalk.jsx/ReadyToTalk.jsx
@@ -4,7 +4,7 @@ const ReadyToTalk = () => {
             <h2 className='text-3xl py-4  text-center'>
                 Ready To Talk?</h2>
                 <p className='text-center text-lg'>Our team is here to answer your question about Sentinel Trust Bank.</p>
-               <div className='my-2 flex justify-center items-center gap-2'>
+               <div className='my-2 flex flex-col lg:flex-row justify-center items-center gap-2'>
                 {/* Link Contact us page */}
                  <button className='btn bg-red-600 hover:bg-red-800 p-2 rounded-lg'>Contact Us</button>
                  {/* Link page to get start */}
