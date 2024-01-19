f9c14631c8e654a53699501b4aeae0a4574fc204
Israt85 <isratanjuman3@gmail.com>
2024-01-19 13:47:24 +0600

    added

--- src/components/About us/AboutCompany/AboutCompany.jsx
+++ src/components/About us/AboutCompany/AboutCompany.jsx
@@ -1,6 +1,6 @@
-import img1 from '../../assets/banner/teamwork (1).jpg'
-import img2 from '../../assets/banner/teamwork.jpg'
-import Container from '../Shared/container/Container';
+import img1 from '../../../assets/banner/teamwork (1).jpg'
+import img2 from '../../../assets/banner/teamwork.jpg'
+import Container from '../../Shared/container/Container';
 
 const AboutCompany = () => {
     return (
@@ -11,8 +11,8 @@ const AboutCompany = () => {
                 <img className='h-60 w-96' src={img2} alt="" />
                 <img className='ml-28 -mt-32 h-60 w-96' src={img1} alt="" />
                 </div>
-                <div className='absolute -mt-32 mx-6 flex gap-2 p-6 bg-white border-4 w-96 lg:w-1/4 border-l-[#4d774e]'>
-                    <h2 className='text-3xl' >25</h2>
+                <div className='absolute hover:scale-110 -mt-32 mx-6 flex gap-2 p-6 bg-white border-4 w-96 lg:w-1/4 border-l-[#4d774e]'>
+                    <h2 className='text-3xl text-red-600 font-bold' >25</h2>
                     <h2 className='w-40'>We Have More Than years of Experience</h2>
                 </div>
 
