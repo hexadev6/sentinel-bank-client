c307d9b96dc008b21f517b45adaf15982e464e02
RSsunny <sunnysharif154@gmail.com>
2024-01-23 10:16:59 +0600

    update

--- src/components/Provider/AuthProvider.jsx
+++ src/components/Provider/AuthProvider.jsx
@@ -1,42 +1,34 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 const auth = getAuth();
-const AuthProvider = ({children}) => {
-    const [user,setUser] = useState(null)
-    const [loading,setLoading] = useState(true)
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  const userSignUp = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const userSignUp = (email,password)=>{
-        setLoading(true)
-       return createUserWithEmailAndPassword(auth,email,password)
-    }
-    const userLogin= (email,password)=>{
-        setLoading(true)
-        return signInWithEmailAndPassword(auth,email,password)
-    }
-
-
-
-
-
-
-
-
-    const authInfo ={
-        user,
-        loading,
-        userSignUp,
-        userLogin,
-
-
-    }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const authInfo = {
+    user,
+    loading,
+    userSignUp,
+    userLogin,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
