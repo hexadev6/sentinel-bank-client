808ec13b03b3462e48a76cfcb48efb6343d22986
Mostak Ahmed <mosak.ahm263@gmail.com>
2024-01-25 18:29:06 +0600

    fixed some bug

--- src/components/About us/Awards/Awards.jsx
+++ src/components/About us/Awards/Awards.jsx
@@ -3,12 +3,12 @@ import award2 from '../../../assets/banner/awards2 - Copy.jpeg'
 import award3 from '../../../assets/banner/awards3 - Copy.jpg'
 import award4 from '../../../assets/banner/award4.png'
 import Container from '../../Shared/container/Container';
-import Heading from '../../Shared/Heading Title/Heading';
+import SubHeading from '../../Shared/Heading Title/SubHeading';
 
 const Awards = () => {
     return (
         <Container>
-            <Heading title={'Our Awards'}></Heading>
+            <SubHeading title={'Our Awards'}></SubHeading>
             <div className='my-4 grid grid-cols-4'>
            <div className='w-60 text-center text-xl font-bold'>
            <img className='h-60 w-60' src={award1} alt="" />
