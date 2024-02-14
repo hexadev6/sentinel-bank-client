9452d0a1ed6af7606893d37dd56ab2d677ab2248
Israt85 <isratanjuman3@gmail.com>
2024-02-14 17:21:17 +0600

    added profil update

--- src/components/Shared/Navbar/ProfileDropdown.jsx
+++ src/components/Shared/Navbar/ProfileDropdown.jsx
@@ -9,19 +9,18 @@ import {
   Avatar,
   
 } from "@material-tailwind/react";
+import empty from '../../../assets/banner/empty image.jpg'
 import {
 
   UserCircleIcon,
  
   ChevronDownIcon,
-  Cog6ToothIcon,
-  InboxArrowDownIcon,
-  LifebuoyIcon,
   PowerIcon,
   
 } from "@heroicons/react/24/solid";
 import useAuth from "../../../Hooks/useAuth";
 import { Link } from "react-router-dom";
+import useStatus from "../../../Hooks/useStatus";
  
 // profile menu component
 const profileMenuItems = [
@@ -38,6 +37,8 @@ const profileMenuItems = [
 function ProfileMenu() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const {user,userLogOut} = useAuth()
+  const userEmail= user?.email
+  const { userinfo} = useStatus({ email: userEmail });
  
   const closeMenu = () => setIsMenuOpen(false);
   const Logout= ()=>{
@@ -49,7 +50,9 @@ function ProfileMenu() {
         console.log(err);
     })
   }
- 
+
+  console.log(userinfo);
+
   return (
     <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
       <MenuHandler>
@@ -58,13 +61,20 @@ function ProfileMenu() {
           color="blue-gray"
           className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
         >
-          <Avatar
-            variant="circular"
-            size="sm"
-            alt="tania andrew"
-            className="border w-12 h-12 border-gray-900"
-            src={user.photoURL}
-          />
+         {
+          userinfo?  <Avatar
+          variant="circular"
+          size="sm"
+          alt="tania andrew"
+          className="border w-12 h-12 border-gray-900"
+          src={userinfo?.image}
+        /> :  <Avatar
+        variant="circular"
+        size="sm"
+        alt="tania andrew"
+        className="border w-12 h-12 border-gray-900"
+        src={empty}
+      />         }
           <ChevronDownIcon
             strokeWidth={2.5}
             className={`h-3 w-3 transition-transform ${
@@ -102,32 +112,6 @@ function ProfileMenu() {
               {label}
             </Typography>
           </MenuItem>
-
-
-
-
-            // <MenuItem
-            //   key={label}
-            //   onClick={closeMenu}
-            //   className={`flex items-center gap-2 rounded ${
-            //     isLastItem
-            //       ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-            //       : ""
-            //   }`}
-            // >
-            //   {React.createElement(icon, {
-            //     className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
-            //     strokeWidth: 2,
-            //   })}
-            //   <Typography onClick={Logout}
-            //     as="span"
-            //     variant="small"
-            //     className="font-normal"
-            //     color={isLastItem ? "red" : "inherit"}
-            //   >
-            //     {label}
-            //   </Typography>
-            // </MenuItem>
           );
         })}
       </MenuList>
--- src/components/UserDialog/UserDialog.jsx
+++ src/components/UserDialog/UserDialog.jsx
@@ -6,26 +6,23 @@ import {
   DialogBody,
   DialogFooter,
   Input,
-  Textarea,
-  Typography,
 } from "@material-tailwind/react";
 import { useForm } from "react-hook-form";
 // import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import Swal from "sweetalert2";
 import { Link } from "react-router-dom";
-
 const UserDialog = ({userinfo,refetch}) => {
+ 
+
     const [open, setOpen] = React.useState(false);
     const axiosSecure= useAxiosSecure()
     const {
         register,
         handleSubmit,
-        reset,
-        formState: { errors },
       } = useForm()
     const handleOpen = () => setOpen(!open);
-    const {_id,name,email,dateOfBirth,mobile,location,nationality}=userinfo || {};
+    const {_id,name, email,dateOfBirth,mobile,location,nationality}=userinfo || {};
     console.log("user",userinfo);
     const onSubmit=async(data)=>{
         const updateProfile={
@@ -37,18 +34,18 @@ const UserDialog = ({userinfo,refetch}) => {
             nationality: data?.nationality
         }
         console.log(updateProfile);
-        const profileRes= await axiosSecure.patch(`/updateUser/${_id}`,updateProfile)
+         await axiosSecure.patch(`/updateUser/${_id}`,updateProfile)
         .then(res=>{
-            console.log(res.data);
-            // if(res.data.data.message=='success'){
-            //     Swal.fire({
-            //         position: "center",
-            //         icon: "success",
-            //         title: "Profile Updated Successfully",
-            //         showConfirmButton: false,
-            //         timer: 1500,
-            //       });
-            // }
+            console.log(res.data.message=='success');
+            if(res.data.message=='success'){
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Profile Updated Successfully",
+                    showConfirmButton: false,
+                    timer: 1500,
+                  });
+            }
            
             refetch()
         })
@@ -58,61 +55,54 @@ const UserDialog = ({userinfo,refetch}) => {
     }
    
     return (
-        <>
-        <div className="flex gap-1 justify-between">
-        <Link to='/'><Button variant="outlined">Go Home</Button ></Link>
-        <Button onClick={handleOpen} className="text-blue-700" variant="text">Edit Profile</Button>
-        <div className="">
-              
-                </div>
+  <>
+      <div className="flex gap-1 justify-between">
+        <Link to='/'>
+          <Button variant="outlined">Go Home</Button>
+        </Link>
+        <Button onClick={handleOpen} className="text-blue-700" variant="text">
+          Edit Profile
+        </Button>
+      </div>
+      <Dialog open={open} size="md" handler={handleOpen}>
+        <div className="flex items-center justify-between">
+          <DialogHeader className="flex flex-col items-start"></DialogHeader>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="mr-3 h-5 w-5"
+            onClick={handleOpen}
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+              clipRule="evenodd"
+            />
+          </svg>
         </div>
-        <Dialog open={open} size="md" handler={handleOpen}>
-            <div className="flex items-center justify-between">
-              <DialogHeader className="flex flex-col items-start">
-                {" "}
-              </DialogHeader>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="mr-3 h-5 w-5"
-                onClick={handleOpen}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogBody>
+            <div className="grid gap-6">
+              <Input {...register('name')} defaultValue={name} label="Username" />
+              <Input {...register('DOB')} defaultValue={dateOfBirth} type="date" label="Date of birth" />
+              <Input disabled {...register('email')} defaultValue={email} label="Email" />
+              <Input {...register('mobile')} defaultValue={mobile} label="Mobile" />
+              <Input {...register('location')} defaultValue={location} label="Location" />
+              <Input {...register('nationality')} defaultValue={nationality} label="Nationality" />
             </div>
-            <form onSubmit={handleSubmit(onSubmit)} >
-            <DialogBody>
-          
-              <div className="grid gap-6">
-               
-
-               <Input {...register("name")} defaultValue={name} label="Username" />
-               <Input {...register("DOB")} defaultValue={dateOfBirth} type="date" label="Date of birth" />
-               <Input disabled {...register("email")} defaultValue={email} label="Email" />
-               <Input {...register("mobile")} defaultValue={mobile} label="Mobile" />
-               <Input {...register("location")} defaultValue={location} label="Location" />
-               <Input {...register("nationality")} defaultValue={nationality} label="Nationality" />
-               
-             </div>
-            
-            </DialogBody>
-            <DialogFooter className="space-x-2">
-              <Button variant="text" color="gray" onClick={handleOpen}>
-                cancel
-              </Button>
-              <Button type="submit" variant="gradient" color="gray">
-                Save
-              </Button>
-            </DialogFooter>
-            </form>
-          
-          </Dialog>
-        </>
+          </DialogBody>
+          <DialogFooter className="space-x-2">
+            <Button variant="text" color="gray" onClick={handleOpen}>
+              Cancel
+            </Button>
+            <Button onClick={handleOpen} variant="gradient" type="submit" color="gray">
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
+      </Dialog>
+    </>
     );
 };
 
