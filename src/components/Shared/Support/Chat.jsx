cab9d5f26e3bc3c7b9f8375e2249dc70223822b8
Israt85 <isratanjuman3@gmail.com>
2024-02-16 15:46:51 +0600

    added socket.io

---
+++ src/components/Shared/Support/Chat.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import './Chat.css';
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+import io from "socket.io-client";
+import useStatus from '../../../Hooks/useStatus';
+import useAuth from '../../../Hooks/useAuth';
+
+const socket = io.connect("http://localhost:3001");
+
+const Chat = () => {
+    const { user } = useAuth()
+    const userEmail = user?.email;
+   
+    const { userinfo, refetch } = useStatus({ email: userEmail });
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        message: currentMessage,
+        time: new Date(Date.now()).toLocaleTimeString(),
+        role: userinfo.status, // Assuming userinfo has a "role" property indicating user or admin
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
+    }
+  };
+
+  useEffect(() => {
+    socket.off("receive_message").on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
+
+  return (
+    <div>
+      <div className="chat-window">
+        <div className="chat-header">
+          <p>Live Chat</p>
+        </div>
+        <div className="chat-body">
+          <div className="message-container">
+            {messageList.map((messageContent, index) => {
+              return (
+                <div key={index}
+                  className={`message ${userinfo.status === 'user' ? 'user' : 'admin'}`}
+                >
+                  <div>
+                    <div className="message-content">
+                      <p>{messageContent.message}</p>
+                    </div>
+                    <div className="message-meta">
+                      <p id="time">{messageContent.time}</p>
+                      <p id="author">{messageContent.role === 'user' ? 'user' : 'admin'}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="chat-footer">
+          <input
+            type="text"
+            value={currentMessage}
+            placeholder="Type here..."
+            onChange={(event) => {
+              setCurrentMessage(event.target.value);
+            }}
+            onKeyPress={(event) => {
+              event.key === "Enter" && sendMessage();
+            }}
+          />
+          <button onClick={sendMessage}>&#9658;</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
