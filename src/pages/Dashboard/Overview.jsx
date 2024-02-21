2fbc668465a91b28c5aca987a98aa3fadd417ca7
Israt85 <isratanjuman3@gmail.com>
2024-02-21 22:40:09 +0600

    added total accounts chart

--- src/pages/Dashboard/Overview.jsx
+++ src/pages/Dashboard/Overview.jsx
@@ -38,7 +38,18 @@ const Overview = () => {
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-y-3 md:gap-5 justify-between items-start p-5">
+     {normalUserInfo?.status== 'admin'?
+     <> <div className="grid grid-cols-1 md:grid-cols-3 gap-y-3 md:gap-5 justify-between items-start p-5">
+     <div className="col-span-2 space-y-6">
+      
+       <UserGraph />
+       
+       <ChatApp  key={user?._id} userId={userId} adminId={adminId} />
+     </div>
+     
+   </div></>  : <>
+     
+     <div className="grid grid-cols-1 md:grid-cols-3 gap-y-3 md:gap-5 justify-between items-start p-5">
         <div className="col-span-2 space-y-6">
           <Transfer />
           <UserGraph />
@@ -47,6 +58,17 @@ const Overview = () => {
         </div>
         <Cards />
       </div>
+     
+     
+     
+     </>
+    
+    
+    
+    
+    
+    
+    }
     </>
   );
 };
