aaa1e8d7ea2db3aa968449ba4686b92031ff9523
Zannatul Ferdousi Hema <shanzidadisha1234@gmail.com>
2024-02-28 23:25:18 +0600

    responsive

--- src/components/Shared/Dasboard Topbar/HidedMenuAdmin.jsx
+++ src/components/Shared/Dasboard Topbar/HidedMenuAdmin.jsx
@@ -6,7 +6,7 @@ import {
   IconButton,
   Switch,
 } from "@material-tailwind/react";
-import { FaBarsStaggered } from "react-icons/fa6";
+import { FaBarsStaggered, FaSnapchat } from "react-icons/fa6";
 import { IoDownloadOutline, IoLogOutOutline } from "react-icons/io5";
 import { TfiWrite } from "react-icons/tfi";
 import { GrTransaction } from "react-icons/gr";
@@ -21,7 +21,7 @@ import useStatus from "../../../Hooks/useStatus";
 import { HiMiniBanknotes } from "react-icons/hi2";
 import { MdSupportAgent } from "react-icons/md";
 
-const HidedMenu = () => {
+const HidedMenuAdmin = () => {
   const [open, setOpen] = React.useState(false);
   const { darkMode, toggleDarkMode } = useDarkMode();
   const pdfContentRef = useRef(null);
@@ -32,7 +32,6 @@ const HidedMenu = () => {
   const openDrawer = () => setOpen(true);
   const closeDrawer = () => setOpen(false);
   const { user, userLogOut } = useAuth();
-  const { userinfo } = useStatus({ email: user?.email });
 
   const Logout = () => {
     userLogOut()
@@ -43,6 +42,7 @@ const HidedMenu = () => {
         console.log(err);
       });
   };
+
   return (
     // drawer menu for sm device
     <React.Fragment>
@@ -93,16 +93,16 @@ const HidedMenu = () => {
         <div className="flex-grow mt-12">
           <div className="w-full flex flex-col gap-2 ">
             <NavLink
-              to="/dashboard/user/overview"
+              to="/dashboard/admin/overview"
               className={({ isActive, isPending }) =>
                 isPending
                   ? "pending"
                   : isActive
                   ? "text-white bg-nevy-blue w-full rounded"
-                  : "hover:bg-gray-200 rounded ease-in duration-500 "
+                  : "hover:bg-gray-200 rounded ease-in duration-500 w-full"
               }
             >
-              <div className="flex  items-center gap-3 px-2 py-1">
+              <div className="flex  items-center gap-3 px-2 py-1 ">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -115,17 +115,32 @@ const HidedMenu = () => {
                     clipRule="evenodd"
                   />
                 </svg>
-                <span className="flex">Overview</span>
+                <span className="flex  ">Dashboard</span>
               </div>
             </NavLink>
             <NavLink
-              to="createAcc"
+              to="notice"
               className={({ isActive, isPending }) =>
                 isPending
                   ? "pending"
                   : isActive
                   ? "text-white bg-nevy-blue w-full rounded"
-                  : "hover:bg-gray-200 rounded ease-in duration-500 "
+                  : "hover:bg-gray-200 rounded ease-in duration-500 w-full"
+              }
+            >
+              <div className="flex  items-center gap-3 px-2 py-1">
+                <BsBank2 />
+                <span className="flex ">Important Notice</span>
+              </div>
+            </NavLink>
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-white bg-nevy-blue w-full rounded"
+                  : "hover:bg-gray-200 rounded ease-in duration-500 w-full"
               }
             >
               <div className="flex  items-center gap-3 px-2 py-1">
@@ -141,76 +156,12 @@ const HidedMenu = () => {
                     clipRule="evenodd"
                   />
                 </svg>
-                <span className="flex">Create Bank Account</span>
-              </div>
-            </NavLink>
-            <NavLink
-              to="quickBanking"
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? "pending"
-                  : isActive
-                  ? "text-white bg-nevy-blue w-full rounded"
-                  : "hover:bg-gray-200 rounded ease-in duration-500 "
-              }
-            >
-              <div className="flex  items-center gap-3 px-2 py-1">
-                <BsBank2 />
-                <span className="flex">Quick Banking</span>
-              </div>
-            </NavLink>
-            {/* <NavLink
-              to="/"
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? "pending"
-                  : isActive
-                  ? "text-white bg-nevy-blue w-full rounded"
-                  : "hover:bg-gray-200 rounded ease-in duration-500 "
-              }
-            >
-              <div className="flex  items-center gap-3 px-2 py-1">
-                <GrTransaction />
-                <span className="flex">Transaction</span>
-              </div>
-            </NavLink> */}
-            <NavLink
-              to="/dashboard/applicationCard"
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? "pending"
-                  : isActive
-                  ? "text-white bg-nevy-blue w-full rounded"
-                  : "hover:bg-gray-200 rounded ease-in duration-500 "
-              }
-            >
-              <div className="flex  items-center gap-3 px-2 py-1">
-                <TfiWrite />
-                <span className="flex">Application Card</span>
+                <span className="flex ">Users Management</span>
               </div>
             </NavLink>
 
             <NavLink
-              to={`${
-                userinfo?.loanStatus?.length === 1
-                  ? `/dashboard/loanOverview/${userinfo?.loanStatus[0].loanId}`
-                  : "/dashboard/applyLoan"
-              }`}
-              className={({ isActive, isPending }) =>
-                isPending
-                  ? "pending"
-                  : isActive
-                  ? "text-white bg-nevy-blue w-full rounded"
-                  : "hover:bg-gray-200 rounded ease-in duration-500 "
-              }
-            >
-              <div className="flex  items-center gap-3 px-2 py-1">
-                <HiMiniBanknotes />
-                <span className="flex">Loan</span>
-              </div>
-            </NavLink>
-            <NavLink
-              to="/dashboard/support"
+              to="/dashboard/accountManagement"
               className={({ isActive, isPending }) =>
                 isPending
                   ? "pending"
@@ -220,34 +171,68 @@ const HidedMenu = () => {
               }
             >
               <div className="flex  items-center gap-3 px-2 py-1">
-                <MdSupportAgent />
-                <span className="flex">Support</span>
+                <GrTransaction />
+                <span className="flex ">Account Management</span>
               </div>
             </NavLink>
             <NavLink
-              to="/my-profile"
+              to="/dashboard/loanManagement"
               className={({ isActive, isPending }) =>
                 isPending
                   ? "pending"
                   : isActive
                   ? "text-white bg-nevy-blue w-full rounded"
-                  : "hover:bg-gray-200 rounded ease-in duration-500 "
+                  : "hover:bg-gray-200 rounded ease-in duration-500 w-full"
               }
             >
               <div className="flex  items-center gap-3 px-2 py-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-5 w-5"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <span className="flex">Setting</span>
+                <GrTransaction />
+                <span className="flex ">Loan Management</span>
+              </div>
+            </NavLink>
+            <NavLink
+              to="/dashboard/cardManagement"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-white bg-nevy-blue w-full rounded"
+                  : "hover:bg-gray-200 rounded ease-in duration-500 w-full"
+              }
+            >
+              <div className="flex  items-center gap-3 px-2 py-1">
+                <GrTransaction />
+                <span className="flex ">Card Management</span>
+              </div>
+            </NavLink>
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-white bg-nevy-blue w-full rounded"
+                  : "hover:bg-gray-200 rounded ease-in duration-500 w-full"
+              }
+            >
+              <div className="flex  items-center gap-3 px-2 py-1">
+                <TfiWrite />
+                <span className="flex ">Reports and Analytics</span>
+              </div>
+            </NavLink>
+            <NavLink
+              to="/dashboard/chat"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-white bg-nevy-blue w-full rounded"
+                  : "hover:bg-gray-200 rounded ease-in duration-500 w-full"
+              }
+            >
+              <div className="flex  items-center gap-3 px-2 py-1">
+                <FaSnapchat />
+                <span className="flex">Chat with Users</span>
               </div>
             </NavLink>
             <NavLink
@@ -309,4 +294,4 @@ const HidedMenu = () => {
   );
 };
 
-export default HidedMenu;
+export default HidedMenuAdmin;
